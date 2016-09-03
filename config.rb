13d7c9e8fd3e8806fd36b0f12dbff2eacd1b6ec4
WendyBeth <wendybeth010@gmail.com>
2016-09-03 13:06:16 -0400

    Push to development by default, clean up config file

--- config.rb
+++ config.rb
@@ -1,33 +1,26 @@
-###
-# Page options, layouts, aliases and proxies
-###
-
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
 
-# Per-page layout changes:
-
-# With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page '/feed.xml', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+activate :deploy do |configure|
+  configure.deploy_method = :git
+  configure.branch = 'master'
+end
 
-###
-# Helpers
-###
 
 activate :blog do |blog|
   blog.sources = "articles/{year}-{month}-{day}-{title}.html"
   blog.taglink = "tags/{tag}.html"
   blog.layout = "blog_layout"
-  blog.summary_length = 0
   blog.year_link = "{year}.html"
   blog.month_link = "{year}/{month}.html"
   blog.day_link = "{year}/{month}/{day}.html"
@@ -36,44 +29,10 @@ activate :blog do |blog|
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
 
-  # Enable pagination
   blog.paginate = true
   blog.per_page = 10
   blog.page_link = "page/{num}"
 end
 
 activate :syntax, :line_numbers => true
-
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  # Optional Settings
-  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
-  deploy.branch = 'master' # default: gh-pages
-  # deploy.strategy = :submodule # commit strategy: can be :force_push or
-  # :submodule
-  # deploy.commit_message = 'custom-message' # default: Automated commit at
-  # timestamp by middleman-deploy version
-end
-
-page "/feed.xml", layout: false
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-end
 
